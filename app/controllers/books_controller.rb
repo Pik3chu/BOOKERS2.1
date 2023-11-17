c148f9f383c9acb74d00c051e0b8d87829851cdc
EC2 Default User <145563566+Pik3chu@users.noreply.github.com>
2023-11-17 06:58:36 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users, only: [:index, :show, :edit, :update]
+
   root to: "homes#top"
 
   get 'home/about' => 'homes#about', as: 'about'
 
-
-  resources :books, only: [:edit, :create, :index, :show, :destroy, :update]
-
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :create, :edit, :show, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 
 end
